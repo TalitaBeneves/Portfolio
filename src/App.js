aa339c7b356526a090499c16a95d18a28eb2bf29
Talita Beneves <talitabeneves15@gmail.com>
2021-11-21 00:39:17 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import { Section } from '../src/styles/GlobalComponents';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Hero/>
+        <Header/>
+      <Section grid>
+        <Hero/>
+      </Section>
     </div>
   );
 }
